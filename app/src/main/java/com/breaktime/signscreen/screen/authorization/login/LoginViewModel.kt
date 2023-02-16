9df419f4eddb69f0c83e8caf20172896077f8b65
sssplinter <kristinasementsova21@gmail.com>
2023-02-16 13:21:14 +0300

    implement login useCase usage in Login View Model

--- app/src/main/java/com/breaktime/signscreen/screen/authorization/login/LoginViewModel.kt
+++ app/src/main/java/com/breaktime/signscreen/screen/authorization/login/LoginViewModel.kt
@@ -1,35 +1,52 @@
 package com.breaktime.signscreen.screen.authorization.login
 
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.breaktime.signscreen.domain.authorization.LoginUseCase
 import com.breaktime.signscreen.utils.isValidEmail
 import com.breaktime.signscreen.utils.isValidPassword
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
+    // TODO replace by injection
+    private val loginUseCase: LoginUseCase = LoginUseCase()
+
     var login by mutableStateOf("")
-    var isValidLogin by mutableStateOf(true)
+//    var isValidLogin by mutableStateOf(true)
 
     var password by mutableStateOf("")
-    var isValidPassword by mutableStateOf(true)
+//    var isValidPassword by mutableStateOf(true)
+
+    var token by mutableStateOf("")
+
+    var errorMessage by mutableStateOf("")
 
     fun onLoginValueChange(value: String) {
         login = value
-        isValidLogin = login.isNotBlank()
+//        isValidLogin = login.isNotBlank()
     }
 
     fun onPasswordValueChange(value: String) {
         password = value
-        isValidPassword = password.isNotBlank()
+//        isValidPassword = password.isNotBlank()
     }
 
     fun onLoginClick() {
-        isValidPassword = isValidPassword(password)
-        isValidLogin = isValidEmail(login) && login.isNotBlank()
-        if( isValidLogin && isValidPassword){
-            // TODO request
+        when {
+            !isValidPassword(password) -> {
+                errorMessage = "Password must contain at least 8 characters"
+            }
+            !isValidEmail(login) -> {
+                errorMessage = "Incorrect email"
+            }
+            else -> {
+                viewModelScope.launch {
+                    token = loginUseCase(login, password)?.token ?: ""
+                }
+            }
         }
     }
 }
