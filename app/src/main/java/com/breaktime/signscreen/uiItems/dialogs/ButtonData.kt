6ce1b3943e9397d5448ad99341a7521272ab9b9f
sssplinter <kristinasementsova21@gmail.com>
2023-02-21 14:10:55 +0300

    create icon button data class

--- app/src/main/java/com/breaktime/signscreen/uiItems/dialogs/ButtonData.kt
+++ app/src/main/java/com/breaktime/signscreen/uiItems/dialogs/ButtonData.kt
@@ -1,6 +1,13 @@
 package com.breaktime.signscreen.uiItems.dialogs
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class ButtonData(
     val title: String,
+    val onClick: () -> Unit
+)
+
+data class IconButtonData(
+    val imageVector: ImageVector,
     val onClick: () -> Unit
 )
