cdc7965a48d6cfb43950e2992c9f76e2fd6e520a
sssplinter <kristinasementsova21@gmail.com>
2023-03-06 11:35:52 +0300

    create application and apply Dagger Component initialization

---
+++ app/src/main/java/com/breaktime/signscreen/App.kt
@@ -0,0 +1,21 @@
+package com.breaktime.signscreen
+
+import android.app.Application
+import android.content.Context
+import com.breaktime.signscreen.di.AppComponent
+import com.breaktime.signscreen.di.DaggerAppComponent
+
+class App : Application() {
+    lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.create()
+    }
+}
+
+val Context.appComponent: AppComponent
+    get() = when (this) {
+        is App -> appComponent
+        else -> this.applicationContext.appComponent
+    }
