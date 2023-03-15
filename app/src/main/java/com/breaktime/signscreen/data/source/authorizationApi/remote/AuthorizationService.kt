488c1e567fcd69545dd2e617e6e5fac1e75b82b6
sssplinter <kristinasementsova21@gmail.com>
2023-03-15 12:55:10 +0300

    implement logic for authorization requests to not to use header with token

--- app/src/main/java/com/breaktime/signscreen/data/source/authorizationApi/remote/AuthorizationService.kt
+++ app/src/main/java/com/breaktime/signscreen/data/source/authorizationApi/remote/AuthorizationService.kt
@@ -4,13 +4,15 @@ import com.breaktime.signscreen.data.network.models.TokenResponse
 import com.breaktime.signscreen.data.network.models.UserLoginRequestInfo
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
-
 
 interface AuthorizationService {
     @POST("login")
+    @Headers("No-Authentication: true")
     suspend fun login(@Body user: UserLoginRequestInfo): Response<TokenResponse>
 
     @POST("register")
+    @Headers("No-Authentication: true")
     suspend fun register(@Body user: UserLoginRequestInfo): Response<TokenResponse>
 }
