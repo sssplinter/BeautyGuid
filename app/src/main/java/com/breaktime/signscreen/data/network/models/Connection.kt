8c1341770498050edcfa5ec3f4b3912a0d586563
sssplinter <kristinasementsova21@gmail.com>
2023-03-15 12:54:17 +0300

    implement service interceptor usage for existed retrofit instance, make retrofit instance a singleton

--- app/src/main/java/com/breaktime/signscreen/data/network/models/Connection.kt
+++ app/src/main/java/com/breaktime/signscreen/data/network/models/Connection.kt
@@ -1,14 +1,29 @@
 package com.breaktime.signscreen.data.network.models
 
+import com.breaktime.signscreen.data.pref.SharedPreferenceRepository
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    // for https use other ip
+    private var retrofit: Retrofit? = null
+
+    // TODO for https use other ip
     private const val baseUrl = "http://10.0.2.2:8080/"
 
-    fun getInstance(): Retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    fun getInstance(sharedPreferenceRepository: SharedPreferenceRepository): Retrofit {
+        if (retrofit == null) {
+            val client = OkHttpClient.Builder()
+                .addInterceptor(ServiceInterceptor(sharedPreferenceRepository))
+                .build()
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+        return retrofit ?: throw IllegalStateException()
+    }
 }
