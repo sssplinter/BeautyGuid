324b43b9a51e3f34ce1446a60d6056fe32df47b4
sssplinter <kristinasementsova21@gmail.com>
2023-03-23 15:37:54 +0300

    redesign salons preview list

---
+++ app/src/main/java/com/breaktime/signscreen/screen/appointments/salons/SalonsList.kt
@@ -0,0 +1,35 @@
+package com.breaktime.signscreen.screen.appointments.salons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.breaktime.signscreen.R
+import com.breaktime.signscreen.data.entities.SalonInfo
+import com.breaktime.signscreen.screen.salonList.SalonListItem
+import com.breaktime.signscreen.ui.theme.BackgroundGray
+
+@Composable
+fun SalonsList(
+    modifier: Modifier = Modifier,
+    salonsListViewModel: SalonsListViewModel,
+    onSalonClick: (String) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier.background(MaterialTheme.colors.BackgroundGray),
+    ) {
+        items(salonsListViewModel.salons) { salonPreview ->
+            SalonListItem(
+                salonInfo = SalonInfo(
+                    salonId = salonPreview.salonId,
+                    salonName = salonPreview.salonName,
+                    salonDescription = salonPreview.salonDescription,
+                    imageId = R.drawable.im_nails3,
+                    categories = salonPreview.categories
+                ),
+                onSalonClick = {  })
+        }
+    }
+}
