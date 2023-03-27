20ef0bcff1bf00c6dd83551228595ee08c2f499d
sssplinter <kristinasementsova21@gmail.com>
2023-03-27 12:05:51 +0300

    create coil image composable to load photo from server

---
+++ app/src/main/java/com/breaktime/signscreen/uiItems/image/CoilImage.kt
@@ -0,0 +1,16 @@
+package com.breaktime.signscreen.uiItems.image
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
+
+@Composable
+fun CoilImage(imageUrl: String, modifier: Modifier = Modifier, contentDescription: String? = null) {
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        contentScale = ContentScale.Crop
+    )
+}
---
+++ app/src/main/java/com/breaktime/signscreen/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.breaktime.signscreen.utils
+
+object Constants {
+    const val salonPhotoPathPrefix = "http://10.0.2.2:8080/salonImage/"
+}
